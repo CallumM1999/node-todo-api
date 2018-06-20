ae89a886824a0455c2d833b6ba594cf743f81762
= <=>
2018-06-20 02:29:06 -0400

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ app.post('/todos', (req, res) => {
     doc => res.send(doc),
     err => res.status(400).send(err)
   );
-})
+});
 
 app.listen(3000, () => console.log('Started on port 3000'));
+
+module.exports = {app};
