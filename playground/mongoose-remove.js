3c0da6d0f6728036ffe1f64a0f976f68356693e4
= <=>
2018-06-21 18:41:59 +0100

    Added DELETE /todos/:id rout

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,8 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {user} = require('./../server/models/user');
+
+
+Todo.findOneAndRemove()
